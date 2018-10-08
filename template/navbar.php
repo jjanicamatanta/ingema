5f7d8abc2b1b888fb9e55f439d2751cd5d6f51b8
Jose Junior Anicama Tanta <j.j.anicamatanta@gmail.com>
2018-10-08 10:41:18 -0500

    [descripcion]

--- template/navbar.php
+++ template/navbar.php
@@ -1,11 +1,11 @@
-<?php 
-  if($title == "Ingema - Ingenieria y Medio Ambiente"){
+<?php
+if ($title == "Ingema - Ingenieria y Medio Ambiente") {
     $nb_transparent = "navbar-transparent";
-  }else{
+} else {
     $nb_transparent = "";
-  }
- ?>
-<nav class="navbar navbar-custom navbar-fixed-top <?php echo($nb_transparent) ?>" role="navigation">
+}
+?>
+<nav class="navbar navbar-custom navbar-fixed-top <?php echo ($nb_transparent) ?>" role="navigation">
   <div class="container">
     <div class="navbar-header">
       <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand p-0" href="index.html">
@@ -14,14 +14,14 @@
     </div>
     <div class="collapse navbar-collapse" id="custom-collapse">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="../../Ingema">Ingema</a></li>
+        <li><a href="/">Ingema</a></li>
         <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Nosotros</a>
           <ul class="dropdown-menu">
-            <li><a href="../../Ingema">Ingema</a></li>
-            <li><a href="../../Ingema/#about">Acerca de nosotros</a></li>
-            <li><a href="../../Ingema/#mejor_opcion">La mejor opción</a></li>
-            <li><a href="../../Ingema#team">Nuestro Equipo</a></li>
-            <li><a href="../../Ingema#clients">Nuestros Clientes</a></li>
+            <li><a href="/">Ingema</a></li>
+            <li><a href="/#about">Acerca de nosotros</a></li>
+            <li><a href="/#mejor_opcion">La mejor opción</a></li>
+            <li><a href="/#team">Nuestro Equipo</a></li>
+            <li><a href="/#clients">Nuestros Clientes</a></li>
           </ul>
         </li>
         <li  class="dropdown"><a class="dropdown-toggle" href="../../Ingema/services.php"  data-toggle="dropdown">Nuestros Servicios</a>
